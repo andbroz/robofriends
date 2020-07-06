0fe6c41b80f4e33f3a2710e4d2162f2715bfd983
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-07-06 23:37:05 +0200

    add Search and filter function

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			robots: robots,
+			searchfield: '',
+		};
+		this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
+	onSearchChange(event) {
+		this.setState({ searchfield: event.target.value });
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name
+				.toLowerCase()
+				.includes(this.state.searchfield.toLowerCase());
+		});
+		return (
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList robots={filteredRobots} />
+			</div>
+		);
+	}
+}
+
+export default App;
